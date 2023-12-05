41e8cc8d8b7782696d9c39ac2c092ab0b0589cf2
Bader alotaibi <baderalotaibi3@gmail.com>
2023-12-05 18:27:24 +0300

    models

--- saudi_reddit/Fourm/models.py
+++ saudi_reddit/Fourm/models.py
@@ -3,17 +3,40 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Subreddit(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/headers/', blank=True)
+    slug = models.SlugField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
+    Subreddit = models.ForeignKey(Subreddit, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
     content = models.TextField()
+    image = models.ImageField(upload_to='images/posts/', blank=True)
+    video = models.FileField(upload_to='videos/posts/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     content = models.TextField()
+    slug = models.SlugField(max_length=255)
+    image = models.ImageField(upload_to='images/comments/', blank=True)
+    video = models.FileField(upload_to='videos/comments/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.content
 
