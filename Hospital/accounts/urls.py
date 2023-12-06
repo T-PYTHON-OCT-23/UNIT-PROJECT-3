42e225b9eb4eac12079bc194df2202ad5f391eae
naif <iimnaif@gmail.com>
2023-12-06 11:39:45 +0300

    1

--- Hospital/accounts/urls.py
+++ Hospital/accounts/urls.py
@@ -5,6 +5,6 @@ app_name = "accounts"
 
 urlpatterns = [
     path("register/", views.register_user_view, name="register_user_view"),
-    path("login/", views.login_user_view, name="login_user_view"),
+    path("accounts/login/", views.login_user_view, name="login_user_view"),
     path("logout/", views.logout_user_view, name="logout_user_view")
 ]
